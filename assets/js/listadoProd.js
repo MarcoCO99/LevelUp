dee9f3f6791f9780c5b4e0c39fda2164f5cf72c9
MarcoCO99 <marc.contrerasa@duocuc.cl>
2025-09-09 23:17:01 -0300

    cambio de rama

---
+++ assets/js/listadoProd.js
@@ -0,0 +1,12 @@
+const productos = [
+  { id: 1, nombre: "PlayStation 5", precio: 549990, categoria: "Consolas", marca: "sony", imagen: "assets/img/ps5.png" },
+  { id: 2, nombre: "Mando Xbox Series X", precio: 59990, categoria: "Consolas", marca: "microsoft", imagen: "assets/img/mando xbox.jpg" },
+  { id: 3, nombre: "Catan", precio: 299990, categoria: "JuegosDeMesa", marca: "Devir", imagen: "assets/img/catan.png" },
+  { id: 4, nombre: "Mouse Gamer Logitech G502", precio: 49990, categoria: "Mouse", marca: "logitech", imagen: "assets/img/logitech g502 hero.jpg" },
+  { id: 5, nombre: "Silla Gamer Secretlab", precio: 349990, categoria: "Sillas", marca: "secretlab", imagen: "assets/img/PRODUCTOS_silla_secretlab.jpg" },
+  { id: 6, nombre: "Carcassone", precio: 24990, categoria: "JuegosDeMesa", marca: "Devir", imagen: "assets/img/carcassonne.jpg" },
+  { id: 7, nombre: "Auriculares Gamer HyperX Cloud II", precio: 79990, categoria: "Accesorios", marca: "Hyper X", imagen: "assets/img/hyperx_cloud_ii_gunmetal_1_main.jpg" },
+  { id: 8, nombre: "PC Gamer ASUS ROG Strix", precio: 1299990, categoria: "Computadores", marca: "ASUS", imagen: "assets/img/ROG strix.jpg" },
+  { id: 9, nombre: "Mousepad Razer Goliathus Extended Chroma", precio: 29990, categoria: "Mousepad", marca: "Razer", imagen: "assets/img/Razer-Goliathus-Extended-Chroma.jpg" },
+  { id: 10, nombre: "Polera Gamer Personalizada 'Level-Up'", precio: 14990, categoria: "Ropa", marca: "microsoft", imagen: "assets/img/polera_LEVELUP.png" },
+];
